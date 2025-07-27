7def968cf87564f2c2d9e6496485c6a36d2809d6
Chrisiverrr <decinillachrisiver@gmail.com>
2025-07-27 16:04:14 +0800

    Create reader.js

---
+++ scripts/reader.js
@@ -0,0 +1,19 @@
+const id = new URLSearchParams(location.search).get('id');
+const ch = +new URLSearchParams(location.search).get('c') || 1;
+const totalPages = 12; // stub
+
+let page = 1;
+const img = document.getElementById('pageImg');
+const info = document.getElementById('pageInfo');
+const prev = document.getElementById('prevZone');
+const next = document.getElementById('nextZone');
+
+function loadPage(){
+  img.src = `images/chapters/${id}/${ch}/${page}.jpg`;
+  info.textContent = `${page} / ${totalPages}`;
+  document.title = `Ch ${ch} P ${page} – Waguri Scan`;
+}
+loadPage();
+
+prev.onclick = ()=>{ if(page>1){page--;loadPage()} };
+next.onclick = ()=>{ if(page<totalPages){page++;loadPage()} };
